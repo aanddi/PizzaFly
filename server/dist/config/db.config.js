adeca27507d9880433b85f50afc64e96720a6538
aanddi <kuropatkinn.a.i21@gmail.com>
2024-04-09 20:17:37 +0300

    deploy

--- server/dist/config/db.config.js
+++ server/dist/config/db.config.js
@@ -10,8 +10,8 @@ import { StopList } from '../entities/stop-list.entity.js';
 import { User } from '../entities/user.entity.js';
 const entities = [Categorie, Product, User, Order, OrderDesc, PromotionList, Promotion, City, StopList];
 const AppDataSource = new DataSource({
-    type: 'mysql',
-    database: 'mydb',
+    type: 'postgres',
+    database: process.env.DATABASE_NAME,
     host: process.env.DATABASE_HOST,
     port: parseInt(process.env.DATABASE_PORT || '3306', 10),
     username: process.env.DATABASE_USER || 'root',
