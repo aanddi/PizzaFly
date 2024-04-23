90cfd615d09db94cf827d16023407f7d4f23a297
aanddi <kuropatkinn.a.i21@gmail.com>
2024-04-23 16:20:33 +0300

    Разработка промокодов

--- server/dist/entities/stop-list.entity.js
+++ server/dist/entities/stop-list.entity.js
@@ -1,63 +1,36 @@
-var __decorate =
-   (this && this.__decorate) ||
-   function (decorators, target, key, desc) {
-      var c = arguments.length,
-         r = c < 3 ? target : desc === null ? (desc = Object.getOwnPropertyDescriptor(target, key)) : desc,
-         d
-      if (typeof Reflect === 'object' && typeof Reflect.decorate === 'function')
-         r = Reflect.decorate(decorators, target, key, desc)
-      else
-         for (var i = decorators.length - 1; i >= 0; i--)
-            if ((d = decorators[i])) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r
-      return c > 3 && r && Object.defineProperty(target, key, r), r
-   }
-var __metadata =
-   (this && this.__metadata) ||
-   function (k, v) {
-      if (typeof Reflect === 'object' && typeof Reflect.metadata === 'function') return Reflect.metadata(k, v)
-   }
-import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
-import { City } from './city.entity.js'
-import { Product } from './product.entity.js'
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { City } from './city.entity.js';
+import { Product } from './product.entity.js';
 let StopList = class StopList {
-   id
-   //=========== СВЯЗЬ ===========//
-   city
-   product
-}
-__decorate(
-   [PrimaryGeneratedColumn({ name: 'stop_list_id' }), __metadata('design:type', Number)],
-   StopList.prototype,
-   'id',
-   void 0
-)
-__decorate(
-   [
-      ManyToOne(
-         () => City,
-         city => city.stopLists,
-         { nullable: false }
-      ),
-      JoinColumn({ name: 'city_id' }),
-      __metadata('design:type', Object)
-   ],
-   StopList.prototype,
-   'city',
-   void 0
-)
-__decorate(
-   [
-      ManyToOne(
-         () => Product,
-         product => product.stopLists,
-         { nullable: false }
-      ),
-      JoinColumn({ name: 'product_id' }),
-      __metadata('design:type', Object)
-   ],
-   StopList.prototype,
-   'product',
-   void 0
-)
-StopList = __decorate([Entity()], StopList)
-export { StopList }
+    id;
+    //=========== СВЯЗЬ ===========//
+    city;
+    product;
+};
+__decorate([
+    PrimaryGeneratedColumn({ name: 'stop_list_id' }),
+    __metadata("design:type", Number)
+], StopList.prototype, "id", void 0);
+__decorate([
+    ManyToOne(() => City, city => city.stopLists, { nullable: false }),
+    JoinColumn({ name: 'city_id' }),
+    __metadata("design:type", Object)
+], StopList.prototype, "city", void 0);
+__decorate([
+    ManyToOne(() => Product, product => product.stopLists, { nullable: false }),
+    JoinColumn({ name: 'product_id' }),
+    __metadata("design:type", Object)
+], StopList.prototype, "product", void 0);
+StopList = __decorate([
+    Entity()
+], StopList);
+export { StopList };
--- server/dist/routes/user.route.js
+++ server/dist/routes/user.route.js
@@ -1,7 +1,7 @@
-import express from 'express'
-import { UserService } from '../services/user.services.js'
-const router = express.Router()
-router.get('/', UserService.getUsers)
-router.get('/get/:phone', UserService.getUserByPhone)
-router.put('/edit/:id', UserService.edit)
-export default router
+import express from 'express';
+import { UserService } from '../services/user.services.js';
+const router = express.Router();
+router.get('/', UserService.getUsers);
+router.get('/get/:phone', UserService.getUserByPhone);
+router.put('/edit/:id', UserService.edit);
+export default router;
